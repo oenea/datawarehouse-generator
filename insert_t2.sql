2692ec95fdb3344249a41de503c079468b5eeb3b
oenea <oenear@gmail.com>
2024-12-01 15:44:10 +0100

    changed inserting directories for t2

--- insert_t2.sql
+++ insert_t2.sql
@@ -1,9 +1,9 @@
 USE CompanyDatabase;
 GO
 
-BULK INSERT Products FROM 'C:\Users\oenea\Documents\SQL Server Management Studio\generator\output\Products_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT Complaint FROM 'C:\Users\oenea\Documents\SQL Server Management Studio\generator\output\Complaints_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT ReturnProcessing FROM 'C:\Users\oenea\Documents\SQL Server Management Studio\generator\output\ReturnProcessing_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT ReturnsTable FROM 'C:\Users\oenea\Documents\SQL Server Management Studio\generator\output\Returns_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT Products FROM 'C:\Users\oenea\Downloads\Generator\output\Products_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT Complaint FROM 'C:\Users\oenea\Downloads\Generator\output\Complaints_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT ReturnProcessing FROM 'C:\Users\oenea\Downloads\Generator\output\ReturnProcessing_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT ReturnsTable FROM 'C:\Users\oenea\Downloads\Generator\output\Returns_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
 
 GO
