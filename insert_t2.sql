51609ab7db313b817cdadec554534a20d1d522b0
oenea <oenear@gmail.com>
2024-12-08 17:23:28 +0100

    resolving issues

--- insert_t2.sql
+++ insert_t2.sql
@@ -2,10 +2,9 @@
 USE CompanyDatabase;
 GO
 
-BULK INSERT Products FROM 'C:\Users\oenea\Downloads\DataWarehouse-generator/output/Products_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT Complaint FROM 'C:\Users\oenea\Downloads\DataWarehouse-generator/output/Complaints_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT ReturnProcessing FROM 'C:\Users\oenea\Downloads\DataWarehouse-generator/output/ReturnProcessing_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT ReturnsTable FROM 'C:\Users\oenea\Downloads\DataWarehouse-generator/output/Returns_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
-BULK INSERT ProductCatalogue FROM 'C:\Users\oenea\Downloads\DataWarehouse-generator/output/ProductCatalogue_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT Complaint FROM 'G:\datawarehouse-generator/output/Complaints_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT ReturnProcessing FROM 'G:\datawarehouse-generator/output/ReturnProcessing_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT ReturnsTable FROM 'G:\datawarehouse-generator/output/Returns_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
+BULK INSERT ProductCatalogue FROM 'G:\datawarehouse-generator/output/ProductCatalogue_t2.csv' WITH (fieldterminator=',',rowterminator='\n', DATAFILETYPE = 'widechar', CODEPAGE = '65001')
 
 GO
